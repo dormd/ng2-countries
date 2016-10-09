f1f209615e4c012a5583779894b98d280a4e30c4
dormd <dormoshed@gmail.com>
2016-10-09 23:03:57 +0300

    country alpha2 to capital pipe

---
+++ src/app/pipes/a2-to-capital.pipe.ts
@@ -0,0 +1,19 @@
+import { Inject, Pipe, PipeTransform } from '@angular/core';
+import { DecimalPipe } from '@angular/common';
+import { COUNTRIES_DATA, Countries, ICountry } from '../models/countries.model';
+
+@Pipe({
+    name: 'a2ToCapital'
+})
+
+export class A2ToCapitalPipe implements PipeTransform {
+    constructor(@Inject(COUNTRIES_DATA) private _countriesData: Countries) {}
+
+    transform(value: any, args: any[]): any {
+        const country: ICountry = this._countriesData[value];
+        if (!country)
+            return '';
+
+        return country.capital || '';
+    } 
+} 
