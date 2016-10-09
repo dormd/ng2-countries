dae035cc62a3009675b84847c3455a5619abd7d9
dormd <dormoshed@gmail.com>
2016-10-09 23:04:27 +0300

    country alpha 2 to countinent(s) pipe

---
+++ src/app/pipes/a2-to-continent.pipe.ts
@@ -0,0 +1,24 @@
+import { Inject, Pipe, PipeTransform } from '@angular/core';
+import { DecimalPipe } from '@angular/common';
+
+import * as _ from "lodash";
+
+import { COUNTRIES_DATA, Countries, ICountry } from '../models/countries.model';
+
+@Pipe({
+    name: 'a2ToContinent'
+})
+
+export class A2ToContinentPipe implements PipeTransform {
+    constructor(@Inject(COUNTRIES_DATA) private _countriesData: Countries) {}
+
+    transform(value: any, args: any[]): any {
+        const country: ICountry = this._countriesData[value];
+        if (!country)
+            return '';
+
+        const continentObj = country.geo.continent;
+        // for better safety (country over two continents)
+        return _.values(continentObj).join(', ');
+    } 
+} 
