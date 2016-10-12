c1b2defb302e849799e3d8c00915e37a5786c202
dormd <dormoshed@gmail.com>
2016-10-12 20:57:21 +0300

    add alpha3 to alpha2 pipe. support 1 or more country at a time

--- src/app/pipes/index.ts
+++ src/app/pipes/index.ts
@@ -2,5 +2,6 @@ export * from './a2-to-capital.pipe';
 export * from './a2-to-continent.pipe';
 export * from './a2-to-country.pipe';
 export * from './a2-to-anthem.pipe';
+export * from './a3-to-a2.pipe'
 export * from './safe.pipe';
 // export * from './country-to-a2.pipe';
