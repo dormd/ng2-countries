73ab91afef8dc8989dd1bd0ef10d16f725aed548
dormd <dormoshed@gmail.com>
2016-10-15 23:31:14 +0300

    add country native name speaker component - use speaker component and speaker services for send request and listen to the native language country name by the speaker

---
+++ src/app/modules/speaker/components/country-native-name-speaker/country-native-name-speaker.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { SpeakerService,
+         CountryNativeNameService } from '../../services';
+
+@Component({
+    selector: 'countr-native-name-speaker',
+    templateUrl: './country-native-name-speaker.component.html'
+})
+
+export class CountryNativeNameSpeakerComponent implements OnInit {
+    @Input() alpha2: string;
+    @Input() gender = 'Female';
+
+    private _name: string = '';
+    private _language: string = '';
+
+    constructor(private _speakerService: SpeakerService,
+                private _countryNativeNameService: CountryNativeNameService) {}
+
+    public ngOnInit() {
+        const nativeNameData = this._countryNativeNameService.getNativeData(this.alpha2);
+        this._name = nativeNameData.name;
+        this._language = nativeNameData.language;
+    }
+}
---
+++ src/app/modules/speaker/services/country-native-name.service.ts
@@ -0,0 +1,34 @@
+import { Injectable,
+         Inject }            from '@angular/core';
+
+import { COUNTRIES_DATA, 
+         Countries }         from '../../shared/models';
+
+import { SpeakerService }    from './speaker.service';
+
+export interface ICountryNativeName {
+    name: string,
+    language: string
+}
+
+@Injectable()
+export class CountryNativeNameService {
+
+    constructor(@Inject(COUNTRIES_DATA) private _countriesData: Countries,
+                private _speakerService: SpeakerService) { }
+
+    public getNativeData(alpha2: string): ICountryNativeName {
+        const nativeNames = this._countriesData[alpha2].name.native;
+        const a3Languages = _.keys(nativeNames);
+        if (a3Languages.length === 0)
+            return null;
+
+        const a3Lang = a3Languages[0];
+        console.log(a3Lang, nativeNames[a3Lang].common);
+
+        return {
+            name: nativeNames[a3Lang].common,
+            language: a3Lang
+        };
+    }
+}
