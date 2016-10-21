150be5232e6260b5947eba349377c9ec30843e3e
dormd <dormoshed@gmail.com>
2016-10-21 15:04:29 +0300

    Country outline - add component for country outlines

--- src/app/components/country-outline/country-outline.component.ts
+++ src/app/components/country-outline/country-outline.component.ts
@@ -1,25 +1,30 @@
-import { Component, Input, OnInit }   from '@angular/core';
+import { Component, Input, OnInit, ElementRef, Renderer }   from '@angular/core';
+import { AnimationsService } from '../../modules/shared/services';
 
 @Component({
     selector: 'app-country-outline',
     templateUrl: './country-outline.component.html',
-    // styleUrls: [ './country-outline.component.less' ],
+    styleUrls: [ './country-outline.component.css' ],    
 })
 
 export class CountryOutlineComponent implements OnInit {
     @Input() alpha2: string;
-
+    @Input() height = 30;
+    @Input() width = 30;
+    @Input() isAnimationAllowed = true;
+ 
     private _classes: Object;
 
-    public constructor() { }
+    constructor(private _elementRef: ElementRef,
+                private _renderer: Renderer,
+                private _animationsService: AnimationsService) { }
 
     public ngOnInit() {
-        const countryIconClass = `flag-icon-${this.alpha2.toLowerCase()}`;
 
-        this._classes = {
-            'outline-icon': true,
-        };
+    }
 
-        this._classes[countryIconClass] = 'true';
+    private _onOver(event) {
+        if (this.isAnimationAllowed)
+            this._animationsService.makeBigger(event.target, this._renderer);
     }
 }
